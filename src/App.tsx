de2ab840eba9e83546cbce72b32e040cb974e649
Andy Smith <61231222+andysm1th@users.noreply.github.com>
2023-03-08 14:34:20 +0100

    fix: logo url handling

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import fastifyIndexRaw from './fastify/index?raw'
 import fastifyPackageJsonRaw from './fastify/package.json?raw'
 import readmeRaw from './fastify/README.md?raw'
 
+import logoUrl from './assets/logo.png'
+
 export default function () {
   return (
     <>
       <header>
         <h1>
-          <img alt="NearForm Logo" height={26} src="/logo.png" />
+          <img alt="NearForm Logo" height={26} src={logoUrl} />
           &nbsp;&nbsp;&nbsp;Fastify Playground
         </h1>
       </header>
