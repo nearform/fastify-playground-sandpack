2acde480f47cdc59ff9fef0c0454ae38b0842db6
Andy Smith <61231222+andysm1th@users.noreply.github.com>
2023-03-08 15:52:30 +0100

    feat: simplify Fastify default script

--- src/fastify/index.js
+++ src/fastify/index.js
@@ -1,18 +1,9 @@
 import Fastify from 'fastify'
 
-const start = async function () {
-  const fastify = Fastify()
+const fastify = Fastify()
 
-  fastify.get('/', async () => {
-    return { hello: 'fastify' }
-  })
+fastify.get('/', async () => {
+  return { hello: 'fastify' }
+})
 
-  try {
-    await fastify.listen({ host: '127.0.0.1', port: 3000 })
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-
-start()
+await fastify.listen()
