fd7ee8b11f911a44c398a811c2f4b8a376cb8c5f
Andy Smith <61231222+andysm1th@users.noreply.github.com>
2023-03-08 14:26:28 +0100

    fix: vite repo base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/fastify-playground-sandpack/',
   plugins: [react()]
 })
